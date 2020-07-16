2a1f6977f06f645734fc9ca1a415150946675806
suryadheeshjith <Surya.dheeshjith@gmail.com>
2020-07-16 21:46:06 +0530

    Added metrics file

---
+++ miniPSP/utils/metric_utils.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+def get_iou(target,prediction):
+    intersection = np.logical_and(target, prediction)
+    union = np.logical_or(target, prediction)
+    iou_score = np.sum(intersection) / np.sum(union)
+    return iou_score
+
+def get_mean_accuracy(target,prediction):
+    return np.mean(target==prediction)
+
+def get_class_accuracies(target,prediction,n_classes):
+
+    assert len(target.shape)==4
+    assert len(prediction.shape)==4
+
+    sum =0
+    acc = {}
+    for i in range(n_classes):
+        cur_acc = np.mean(prediction[:,:,:,i]==target[:,:,:,i])
+        sum+=cur_acc
+        acc[i+1] = cur_acc
+    acc['mean'] = sum/5
+    return acc
+
+
+def get_class_IoU(target,prediction,n_classes):
+
+    assert len(target.shape)==4
+    assert len(prediction.shape)==4
+
+    sum =0
+    IoU = {}
+    for i in range(n_classes):
+        cur_iou = get_iou(prediction[:,:,:,i],target[:,:,:,i])
+        sum+=cur_iou
+        IoU[i+1] = cur_iou
+    IoU['mean'] = sum/5
+    return IoU
+
+
+def get_class_F1(target,prediction,n_classes):
+
+    assert len(target.shape)==4
+    assert len(prediction.shape)==4
+
+    sum =0
+    f1 = {}
+    for i in range(n_classes):
+        cur_f1 = metrics.f1_score(prediction[:,:,:,i].reshape(-1,1),target[:,:,:,i].reshape(-1,1))
+        sum+=cur_f1
+        f1[i+1] = cur_f1
+    f1['mean'] = sum/5
+    return f1
+
+
+def evaluate(target,prediction,n_classes):
+    acc = get_class_accuracies(target,prediction,n_classes)
+    iou = get_class_IoU(target,prediction,n_classes)
+    f1 = get_class_F1(target,prediction,n_classes)
+
+    return acc,iou,f1
