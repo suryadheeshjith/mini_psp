c7b8f25e13cc0d71c8e9f7cca55e8e6f667b82c0
suryadheeshjith <Surya.dheeshjith@gmail.com>
2020-07-17 19:32:14 +0530

    Modularised

--- miniPSP/utils/store_utils.py
+++ miniPSP/utils/store_utils.py
@@ -1,4 +1,5 @@
 import os.path as osp
+from io import StringIO
 
 def save_details(args,input_shape,target_shape):
 
@@ -17,3 +18,23 @@ def save_details(args,input_shape,target_shape):
     f.write('Input shape : {}\n'.format(input_shape))
     f.write('Target shape : {}'.format(target_shape))
     f.close()
+
+
+def get_summary_string(model):
+    tmp_smry = StringIO()
+    model.summary(print_fn=lambda x: tmp_smry.write(x + '\n'))
+    summary = tmp_smry.getvalue()
+    return summary
+
+
+def save_model(model, model_path):
+
+    # Save model JSON
+    model_json = model.to_json()
+    save_json_path = osp.join(model_path,"model.json")
+    with open(save_json_path, "w") as json_file:
+        json_file.write(model_json)
+
+    # Save final weights
+    save_weight_path = osp.join(model_path,"model_final_weights.h5")
+    model.save_weights(save_weight_path)
